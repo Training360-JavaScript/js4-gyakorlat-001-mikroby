762e8e592a3aece693c85542c8030cc80bb32d0c
mikroby <rtamas@hotmail.com>
2021-11-17 16:53:19 +0100

    task solved

--- solutions/app1.js
+++ solutions/app1.js
@@ -1,7 +1,12 @@
+const converterPromise = (array = []) =>
 
+  new Promise((resolve, reject) => {
 
+    if (array.every(item => typeof item === 'string')) {
+      resolve(array.map(item => item.toUpperCase()))
+    } else {
+      reject('Error: Not all elements are string type!')
+    }
+  });
 
-
-
-
-// export default converterPromise;
+export default converterPromise;
